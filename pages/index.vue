e14a8bafb427246462d839140ffbefea97052291
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-21 20:59:17 +0100

    Index

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,9 @@
   <section class="intro">
   <h1>Get the Latest Tech News</h1>
   </section>
-  <section>
-  <div class="uk-child-width-expand@s uk-text-center" uk-grid>
-  <PostPreview
-  id="1"
-  thumbnail="https://picsum.photos/500/320?random=1" 
-  title="First post"
-  previewText="This is my first post"/>
-  <PostPreview
-  id="2"
-  thumbnail="https://picsum.photos/500/320?random=2" 
-  title="Second Post"
-  previewText="This is my second post"/>
-  <PostPreview
-  id="3"
-  thumbnail="https://picsum.photos/500/320?random=3" 
-  title="Third post"
-  previewText="This is my third post"/>
-</div>
-</section>
-</div>
+ <PostList />
+  </div>
+
 <!-- card -->
 </template>
 
