bc228305cf62e932b8d90181e239ff472c3d1011
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-22 18:04:06 +0100

    Screenshots

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -20,7 +20,6 @@
 <script>
 export default {
   name: 'AdminAuthPage',
-  layout: 'admin',
   data() {
     return {
       isLogin: true
@@ -28,3 +27,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
