dcbaf634ebcdecca363d8e1b67afbe8dd50570c1
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-22 13:13:36 +0100

    App Components

--- components/AppControlInput.vue
+++ components/AppControlInput.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="input-control">
+  <div class="uk-margin">
     <label><slot /></label>
     <input
+      class="uk-input"
       v-if="controlType === 'input'"
       v-bind="$attrs"
       :value="value"
       @input="$emit('input', $event.target.value)">
     <textarea
+      class="uk-textarea"
       v-if="controlType === 'textarea'"
       rows="10"
       :value="value"
--- pages/admin/new-posts/index.vue
+++ pages/admin/new-posts/index.vue
@@ -5,32 +5,12 @@
 <h1 class="uk-heading-bullet">New Posts</h1>
 <form>
     <fieldset class="uk-fieldset">
+       <legend class="uk-legend">Start A New Post</legend>
+       <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+         <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
 
-        <legend class="uk-legend">Star A New Post</legend>
-
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-
-     
-
-        <div class="uk-margin">
-            <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
-        </div>
-
-        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-            <label><input class="uk-radio" type="radio" name="radio2" checked> A</label>
-            <label><input class="uk-radio" type="radio" name="radio2"> B</label>
-        </div>
-
-        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-            <label><input class="uk-checkbox" type="checkbox" checked> A</label>
-            <label><input class="uk-checkbox" type="checkbox"> B</label>
-        </div>
-
-        <div class="uk-margin">
-            <input class="uk-range" type="range" value="2" min="0" max="10" step="0.1">
-        </div>
+      
 
     </fieldset>
 </form>
@@ -41,9 +21,11 @@
 </template>
 
 <script>
+import AppControlInput from '~/components/AppControlInput.vue'
 
 
 export default {
+  components: { AppControlInput },
  
 }
 </script>
