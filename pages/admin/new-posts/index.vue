01087d17f379351a897a4f2d01e99157a9cd5656
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-22 13:39:05 +0100

    Post Methods

--- pages/admin/new-posts/index.vue
+++ pages/admin/new-posts/index.vue
@@ -3,16 +3,21 @@
 <section class="new-posts">
 
 <h1 class="uk-heading-bullet">New Posts</h1>
-<form>
+<form @submit.prevent="onSave">
     <fieldset class="uk-fieldset">
        <legend class="uk-legend">Start A New Post</legend>
        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
         <AppControlInput v-model="editedPost.title">Title</AppControlInput>
          <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
-
-      
-
-    </fieldset>
+         <AppControlInput 
+         control-type="textarea"
+         v-model="editedPost.content">Content</AppControlInput>
+         <AppButton1 type="submit">Save</AppButton1>
+         <AppButton2
+         type="button"
+         style="margin-left: 10px"
+         @click="onCancel">Cancel</AppButton2>
+ </fieldset>
 </form>
 
 </section>
@@ -21,11 +26,25 @@
 </template>
 
 <script>
-import AppControlInput from '~/components/AppControlInput.vue'
-
-
 export default {
-  components: { AppControlInput },
- 
+data () {
+  return {
+       editedPost: {
+       author: '',
+       title: '',
+       thumbnailLink: '',
+       content:''
+
+     }
+  }
+},
+methods: {
+  onSave() {
+    // Save the post
+  },
+  onCancel() {
+    // Navigate back
+  }
+}
 }
 </script>
