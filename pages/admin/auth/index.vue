e2764ff4964c7f6aea8ac516144056746af04ec0
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-24 16:36:06 +0100

    Token

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -30,19 +30,20 @@ export default {
   },
   methods: {
     onSubmit() {
-      this.$axios.$post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.fbAPIKey, {
+     this.$store.dispatch('authenticateUser', {
+       isLogin: this.isLogin,
        email: this.email,
-       password: this.password,
-       returnSecureToken: true 
-      })
-      .then(result => {
-        console.log(result)
-      })
-      .catch(e => console.log(e));
+       password: this.password
+     })
+     .then(() => {
+       this.$router.push('/admin');
+     })
     }
   }
 }
 </script>
+
+
 
 <style scoped>
 
